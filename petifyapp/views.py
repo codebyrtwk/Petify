0172cac4ad7de8226a599315a74f301263336743
codebyrtwk <ritwikbahukhandi98@gmail.com>
2021-07-03 12:06:43 +0530

    First Commit

---
+++ petifyapp/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+def home(request):
+	return render(request, 'petify/home.html')
+
+def signup(request):
+	return render(request , 'petify/signup.html')
+
+def login(request):
+	return render(request , 'petify/login.html')
